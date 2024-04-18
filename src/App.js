221fcf3f46dd810baa2720392691af78a0744f94
Winateewii <wisuda2012@gmail.com>
2024-04-18 21:22:22 +0700

    add tailwind and Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header class="bg-white">
+      <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Wiwi</span>
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          </a>
+        </div>
+        <div class="hidden lg:flex lg:flex-1 lg:justify-end gap-x-12">
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">About Me</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Project</a>
+          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Contact</a>
+        </div>
+      </nav>
+    </header>
+
   );
 }
 
